53551acab738c3410eb3c3e59fa9452c7208f1cf
laujuu <lauroindcontato@gmail.com>
2022-03-16 17:09:02 -0300

    requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui vqv
+function getSpeciesByIds(ids, ids2) {
+  const result = data.species.filter((eachElement) => eachElement.id === ids
+  || eachElement.id === ids2);
+  return result;
 }
-
 module.exports = getSpeciesByIds;
